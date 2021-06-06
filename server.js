5e0aeea9a315919d81de810c6637270f8825c9ef
Hamed Najafi <hamed.najafi.haghi@gmail.com>
2021-06-06 14:26:45 +0430

    a

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ io.on('connection', socket => {
     })
   })
 })
-
-server.listen(3000, () => console.log('server is up at 3000'))
+const port = process.env.PORT || 3000
+server.listen(port, () => console.log(`server is up at ${port}`))
